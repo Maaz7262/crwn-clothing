6440ca694bc59606bf32292cb91bfa70438a5378
Maaz7262 <>
2022-08-17 20:10:43 -0700

    updated naming convention

--- src/components/collection_preview/collection_preview.components.jsx
+++ src/components/collection_preview/collection_preview.components.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import CollectionItem from "../collection_item/collection_item.component";
-
+import { Link, useLocation } from "react-router-dom";
 import './collection_preview.styles.scss'
 
 const CollectionPreview = ({title, items}) =>{
+    let location = useLocation();
     return(
 
     <div className="collection-preview">
-        <h1 className="title">{title.toUpperCase()}</h1>
+        <Link to={location.pathname+'/'+title.toLowerCase()}><h1 className="title">{title.toUpperCase()}</h1></Link>
         <div className="preview">
             { items.filter((item, idx) => (
                 idx<4
